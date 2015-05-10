3fc1740782cb69aca1a30d810e89ed5bdc354dad
RobertJGabriel <robert_gabriel@outlook.com>
2015-05-10 18:54:16 +0100

    Alert working

--- assests/js/frames.js
+++ assests/js/frames.js
@@ -28,12 +28,26 @@ $("#searchTerm").keyup(function (e) {
 
 
 $("#password").keyup(function (e) {
-    if ($("#password:focus") && (e.keyCode === 13)) {
-        
+    if (($("#password:focus") && (e.keyCode === 13))) {
       checkSetPassword();
     }
  });
 
+$("#passwordTry").click(function() {
+    checkSetPassword();
+ });
+
+
+
+
+
+
+
+
+$("#passwordSave").click(function() {
+    setPassword(document.getElementById("passwordSet").value);
+    $('#complete-dialog').modal('hide');
+ });
 
 $("#home").click(function(){
            frame.src = "http://www.bing.com" ;
@@ -72,7 +86,7 @@ var userInput = document.getElementById("password").value;
      $('#complete-dialog').modal('hide');
      $('#settings-dialog').modal('show');
  }else{
- alert('error');
+     alerts('error','');
  }
     
     
@@ -97,5 +111,76 @@ function setPassword(password) {
     console.log(localStorage.password);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function alerts(status, message) {
+    $("#alert >div").remove();
+    if (status == 'true') {
+      var div = document.createElement("div");
+          div.setAttribute("class", "alert alert-dismissable alert-success");
+          div.setAttribute("role", "alert");
+          div.setAttribute("id", "alertDiv");
+          div.innerHTML = "Awesome, Hold on two seconds " + message;
+      var button = document.createElement("button");
+          button.setAttribute("type", "button");
+          button.setAttribute("class", "close");
+          button.setAttribute("data-dismiss", "alert");
+          button.innerHTML = "x";
+          div.appendChild(button);
+      document.getElementById("alert").appendChild(div);
+      setTimeout(redirect, 2000);
+
+
+    } else if (status == 'error') {
+      var div = document.createElement("div");
+          div.setAttribute("class", "alert alert-dismissable alert-danger");
+          div.setAttribute("role", "alert");
+          div.setAttribute("id", "alertDiv");
+    var button = document.createElement("button");
+          button.setAttribute("type", "button");
+          button.setAttribute("class", "close");
+          button.setAttribute("data-dismiss", "alertDiv");
+          button.innerHTML = "x";
+          div.appendChild(button);
+          div.innerHTML = "Oh snap something is wrong ";
+      document.getElementById("alert").appendChild(div);
+             $('#complete-dialog').modal('hide');
+
+    } else if (status == 'passwordchanged') {
+      var div = document.createElement("div");
+          div.setAttribute("class", "alert alert-dismissable alert-success");
+          div.setAttribute("role", "alert");
+          div.setAttribute("id", "alertDiv");
+          div.innerHTML = "Password Change Successful";
+      var button = document.createElement("button");
+          button.setAttribute("type", "button");
+          button.setAttribute("class", "close");
+          button.setAttribute("data-dismiss", "alert");
+          button.innerHTML = "x";
+          div.appendChild(button);
+      document.getElementById("alert").appendChild(div);
+    }
+  }
 
 
