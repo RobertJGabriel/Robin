68462424a5de96f6857c227d91097ccead5cd3d6
RobertJGabriel <robert_gabriel@outlook.com>
2015-05-10 14:34:35 +0100

    Search Working :)

--- assests/js/frames.js
+++ assests/js/frames.js
@@ -1,4 +1,16 @@
 var frame = document.getElementById("iframe");
+var searchTerm = document.getElementById("searchTerm").value;
+
+
 
 frame.width = window.innerWidth;
 frame.height = "100%";
+
+
+
+$(document).keyup(function (e) {
+    if ($("#searchTerm:focus") && (e.keyCode === 13)) {
+        frame.src = "https://duckduckgo.com/?q=" + document.getElementById("searchTerm").value + "&ia=meanings";
+        console.log(document.getElementById("searchTerm").value);
+    }
+ });
