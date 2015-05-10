8ddba71c4dbafccee7d7c540583be7893d02610a
RobertJGabriel <robert_gabriel@outlook.com>
2015-05-10 14:58:57 +0100

    Refresh,back,forward buttons

--- assests/js/frames.js
+++ assests/js/frames.js
@@ -14,3 +14,28 @@ $(document).keyup(function (e) {
         console.log(document.getElementById("searchTerm").value);
     }
  });
+
+
+
+$(document).keyup(function (e) {
+    if ($("#searchTerm:focus") && (e.keyCode === 13)) {
+        frame.src = "http://www.bing.com/search?q=" + document.getElementById("searchTerm").value ;
+        console.log(document.getElementById("searchTerm").value);
+    }
+ });
+
+
+$("#refresh").click(function(){
+    document.getElementById('iframe').contentWindow.location.reload();
+ });
+
+$("#back").click(function(){
+    document.getElementById('iframe').contentWindow.history.go(-1);
+ });
+
+$("#forword").click(function(){
+    document.getElementById('iframe').contentWindow.history.go(1);
+ });
+
+
+
