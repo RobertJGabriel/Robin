6b064db5eb338963c07fa5b97b35037ba3183d17
RobertJGabriel <robert_gabriel@outlook.com>
2015-06-07 20:34:10 +0100

    Added : Tabbing :ballon:

--- assests/js/lib/frames.js
+++ assests/js/lib/frames.js
@@ -264,3 +264,33 @@ $(function() {
         $(".searcharea").toggleClass("box-change");
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('a.toggle').on('click', function() {
+  $('section').scrollTop(0);
+  $('.contain').toggleClass('active');
+  return false;
+})
+
+$('section').on('click', function() {
+  //  $(this).closest('section').prependTo('.contain');
+  $('section').removeClass('active');
+  $(this).addClass('active');
+  $('.contain').removeClass('active');
+})
