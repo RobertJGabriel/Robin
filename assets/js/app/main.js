5e09274ad4a6ec6b83d0d61e0fd145aca305b198
Robert Gabriel <robert_gabriel@outlook.com>
2016-01-22 23:02:16 +0000

    Add login panel and firebase connection

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -1,4 +1,4 @@
-ljs.load(['http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js', 'assets/js/lib/jquery-1.11.3.js'], 'assets/js/lib/core.js',
+ljs.load(['http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js', 'assets/js/lib/jquery-1.11.3.js', 'assets/js/lib/firebase.js'], 'assets/js/lib/core.js',
     'assets/js/lib/bootstrap.min.js', 'assets/js/lib/ripples.js', 'assets/js/lib/material.js', 'assets/js/lib/cookie.js',
     function () {
 
--- assets/js/lib/cookie.js
+++ assets/js/lib/cookie.js
@@ -1,19 +1,16 @@
- $(document).ready(function () {
-     $(function () {
-         if (window.location.search.indexOf('dave=error') > -1) {
-             showErrorPanel();
-         }
-         if (localStorage.getItem('seen') == (new Date()).getDate()) {
-             showpanel();
-         }
-     });
 
-     function showpanel() {
-         $("#welcome").modal();
-         localStorage.setItem('seen', (new Date()).getDate());
-     }
+$(document).ready(function () {
+    $(function () {
+     
+        if (localStorage.getItem('seen') != (new Date()).getDate()) {
+            showpanel();
+        }
+    });
 
-     function showErrorPanel() {
-         $("#errorModal").modal();
-     }
- });
+    function showpanel() {
+            $("#welcome").modal();
+        localStorage.setItem('seen', (new Date()).getDate());
+    }
+
+  
+});
