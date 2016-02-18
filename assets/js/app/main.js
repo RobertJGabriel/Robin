c8f4fb73d82f4d04c8b454c8a31a26aba6e4aa99
Robert Gabriel <robert_gabriel@outlook.com>
2016-02-18 01:31:26 +0000

    Enabled shortcuts

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -1,5 +1,5 @@
 ljs.load(['http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js', 'assets/js/lib/jquery-1.11.3.js', 'assets/js/lib/firebase.js'], 'assets/js/lib/core.js',
-    'assets/js/lib/bootstrap.min.js', 'assets/js/lib/ripples.js', 'assets/js/lib/material.js', 'assets/js/lib/cookie.js',
+    'assets/js/lib/bootstrap.min.js','assets/js/app/settings.js', 'assets/js/lib/ripples.js', 'assets/js/lib/material.js', 'assets/js/lib/cookie.js',
     function () {
 
         $.material.init();
