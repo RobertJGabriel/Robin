f21d22bad74023696b57b46e3ef429c308cfaabf
Robert James Gabriel <robert_gabriel@outlook.com>
2016-05-28 13:41:24 +0100

    added firebase 3 + rewrites to core2

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -1,17 +1,25 @@
-ljs.load(['http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js', 'assets/js/lib/jquery-1.11.3.js', 'http://www.cornify.com/js/cornify.js', 'assets/js/lib/firebase.js'], 'assets/js/lib/core.js',
-  'assets/js/lib/bootstrap.min.js', 'assets/js/app/settings.js', 'assets/js/lib/ripples.js', 'assets/js/lib/material.js', 'assets/js/lib/cookie.js',
-  function() {
+ljs.load([
+    'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js',
+    'assets/js/lib/jquery-1.11.3.js',
+    'http://www.cornify.com/js/cornify.js',
+    'https://www.gstatic.com/firebasejs/3.0.0/firebase.js'
+    ],
+    'assets/js/core.js',
+    'assets/js/lib/bootstrap.min.js',
+    'assets/js/app/settings.js',
+    'assets/js/lib/ripples.js',
+    'assets/js/lib/material.js',
+    'assets/js/lib/cookie.js',
 
-    $.material.init();
-
-  });
+    function() {
+      $.material.init();
+    });
 
 
 function regexUrlextensioncheck(n) {
 
-  var s = document.URL,
+    var s = document.URL,
     e = new RegExp(n);
+    return e.test(s);
 
-
-  return e.test(s);
 }
